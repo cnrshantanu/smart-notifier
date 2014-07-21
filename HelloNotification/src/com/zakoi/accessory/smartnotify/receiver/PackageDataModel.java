f92e95d2d1215bb09175765ec9e6c10906030135
Shantanu Das <cnrshantanu@gmail.com>
2014-07-22 01:06:11 +0530

    async task added for retrieving package names

---
+++ HelloNotification/src/com/zakoi/accessory/smartnotify/receiver/PackageDataModel.java
@@ -0,0 +1,19 @@
+package com.zakoi.accessory.smartnotify.receiver;
+
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+
+public class PackageDataModel {
+	
+	String appname = "";
+	String pname = "";
+	String versionName = "";
+	int versionCode = 0;
+	Drawable icon;
+	String TAG = "Package Model";
+	
+	public void prettyPrint() {
+        Log.v(TAG,appname + "\t" + pname + "\t" + versionName + "\t" + versionCode);
+	}
+
+}
--- HelloNotification/src/com/zakoi/accessory/smartnotify/receiver/PackageGrabber.java
+++ HelloNotification/src/com/zakoi/accessory/smartnotify/receiver/PackageGrabber.java
@@ -6,27 +6,22 @@ import java.util.List;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.util.Log;
 
 public class PackageGrabber {
-	private String appname = "";
-	private String pname = "";
-	private String versionName = "";
-	private int versionCode = 0;
-	private Drawable icon;
-	private String TAG = "Package Grabber";
-	private Context m_context;
-	    
-	private void prettyPrint() {
-        Log.v(TAG,appname + "\t" + pname + "\t" + versionName + "\t" + versionCode);
-	}
 	
-    public PackageGrabber(Context context){
+	private Context m_context;
+	private AsyncTask m_getPackageTask;
+	ArrayList<PackageDataModel> m_packageList;
+	    
+	public PackageGrabber(Context context){
     	m_context = context;
+    	m_packageList = new ArrayList<PackageDataModel>();
     }
 	
-	public ArrayList<PackageGrabber> getPackages() {
-	    ArrayList<PackageGrabber> apps = getInstalledApps(false); /* false = no system packages */
+	public ArrayList<PackageDataModel> getPackages() {
+	    ArrayList<PackageDataModel> apps = getInstalledApps(false); /* false = no system packages */
 	    final int max = apps.size();
 	    for (int i=0; i<max; i++) {
 	        apps.get(i).prettyPrint();
@@ -34,15 +29,15 @@ public class PackageGrabber {
 	    return apps;
 	}
 
-	private ArrayList<PackageGrabber> getInstalledApps(boolean getSysPackages) {
-	    ArrayList<PackageGrabber> res = new ArrayList<PackageGrabber>();        
+	private ArrayList<PackageDataModel> getInstalledApps(boolean getSysPackages) {
+	    ArrayList<PackageDataModel> res = new ArrayList<PackageDataModel>();        
 	    List<PackageInfo> packs = m_context.getPackageManager().getInstalledPackages(0);
 	    for(int i=0;i<packs.size();i++) {
 	        PackageInfo p = packs.get(i);
 	        if ((!getSysPackages) && (p.versionName == null)) {
 	            continue ;
 	        }
-	        PackageGrabber newInfo = new PackageGrabber(m_context);
+	        PackageDataModel newInfo = new PackageDataModel();
 	        newInfo.appname = p.applicationInfo.loadLabel(m_context.getPackageManager()).toString();
 	        newInfo.pname = p.packageName;
 	        newInfo.versionName = p.versionName;
@@ -51,5 +46,36 @@ public class PackageGrabber {
 	        res.add(newInfo);
 	    }
 	    return res; 
+	}
+	
+	private void getAppsInBackground() {
+		m_getPackageTask = new AsyncTask<Void, Void, ArrayList<PackageDataModel>>() {
+
+			@Override
+			protected ArrayList<PackageDataModel> doInBackground(Void... arg0) {
+				// TODO Auto-generated method stub
+				ArrayList<PackageDataModel> temp_packageList = new ArrayList<PackageDataModel>();
+				List<PackageInfo> packs = m_context.getPackageManager().getInstalledPackages(0);
+			    for(int i=0;i<packs.size();i++) {
+			        PackageInfo p = packs.get(i);
+			        if ((p.versionName == null)) {
+			            continue ;
+			        }
+			        PackageDataModel newInfo = new PackageDataModel();
+			        newInfo.appname = p.applicationInfo.loadLabel(m_context.getPackageManager()).toString();
+			        newInfo.pname = p.packageName;
+			        newInfo.versionName = p.versionName;
+			        newInfo.versionCode = p.versionCode;
+			        newInfo.icon = p.applicationInfo.loadIcon(m_context.getPackageManager());
+			        temp_packageList.add(newInfo);
+			    }
+				return temp_packageList;
+			}
+			
+			@Override
+		    protected void onPostExecute(final ArrayList<PackageDataModel> result) {
+				m_packageList = result; 
+			}
+		};
 	}
 }
