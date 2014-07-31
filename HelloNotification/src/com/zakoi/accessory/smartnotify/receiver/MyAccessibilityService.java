0bc147d5149e1e0d96db00b3fc412a41458310d4
shantanu <cnrshantanu@gmail.com>
2014-07-31 18:29:01 +0530

    Adding custom notifications

--- HelloNotification/src/com/zakoi/accessory/smartnotify/receiver/MyAccessibilityService.java
+++ HelloNotification/src/com/zakoi/accessory/smartnotify/receiver/MyAccessibilityService.java
@@ -67,29 +67,35 @@ public class MyAccessibilityService extends AccessibilityService {
 							+ sourcePackageName + "]");
 					Log.v(TAG, "Broadcasting for "
 							+ Constants.ACTION_CATCH_NOTIFICATION);
-					
-					addData(sourcePackageName, notificationMsg);
-					 
-					/*android.app.Notification m_notify = (android.app.Notification) event.getParcelableData();
+
+					android.app.Notification m_notify = (android.app.Notification) event
+							.getParcelableData();
 					List<String> m_info = getText(m_notify);
-					
-					for(String msg : m_info){
-						Log.i(TAG," notification message info " + msg);
-					}*/
-					
+					String notificationMsg1 = "";
+					String heading = "";
+					int count = 0;
+					for (String msg : m_info) {
+						if (count == 0) {
+							heading = msg;
+							count++;
+						} else
+							notificationMsg1 += msg + "\n";
+						Log.i(TAG, " notification message info " + msg);
+					}
+
+					addData(sourcePackageName, heading, notificationMsg1);
+
 					/*
-					try {
-						Intent mIntent = new Intent(
-								Constants.ACTION_CATCH_NOTIFICATION);
-						mIntent.putExtra(Constants.EXTRA_PACKAGE,
-								sourcePackageName);
-						mIntent.putExtra(Constants.EXTRA_MESSAGE,
-								notificationMsg);
-						MyAccessibilityService.this.getApplicationContext()
-								.sendBroadcast(mIntent);
-					} catch (Exception e) {
-						Log.e(TAG, e.toString());
-					}*/
+					 * try { Intent mIntent = new Intent(
+					 * Constants.ACTION_CATCH_NOTIFICATION);
+					 * mIntent.putExtra(Constants.EXTRA_PACKAGE,
+					 * sourcePackageName);
+					 * mIntent.putExtra(Constants.EXTRA_MESSAGE,
+					 * notificationMsg);
+					 * MyAccessibilityService.this.getApplicationContext()
+					 * .sendBroadcast(mIntent); } catch (Exception e) {
+					 * Log.e(TAG, e.toString()); }
+					 */
 				} else {
 					Log.e(TAG,
 							"Notification Message is empty. Can not broadcast");
@@ -141,7 +147,7 @@ public class MyAccessibilityService extends AccessibilityService {
 		this.setServiceInfo(info);
 	}
 
-	public void addData(String package_name, String message) {
+	public void addData(String package_name, String heading, String message) {
 		long time = System.currentTimeMillis();
 		long sourceId = NotificationUtil.getSourceId(this,
 				HelloNotificationExtensionService.EXTENSION_SPECIFIC_ID);
@@ -172,6 +178,7 @@ public class MyAccessibilityService extends AccessibilityService {
 			eventValues.put(Notification.EventColumns.DISPLAY_NAME, app_name);
 			eventValues.put(Notification.EventColumns.MESSAGE, message);
 			eventValues.put(Notification.EventColumns.PERSONAL, 1);
+			eventValues.put(Notification.EventColumns.TITLE, heading);
 			eventValues.put(Notification.EventColumns.PROFILE_IMAGE_URI,
 					profileImage);
 			eventValues.put(Notification.EventColumns.PUBLISHED_TIME, time);
@@ -183,74 +190,80 @@ public class MyAccessibilityService extends AccessibilityService {
 	}
 
 	@SuppressLint("NewApi")
-	public static List<String> getText(android.app.Notification notification)
-	{
-	    // We have to extract the information from the view
-	    RemoteViews        views = notification.bigContentView;
-	    if (views == null) views = notification.contentView;
-	    if (views == null) return null;
+	public static List<String> getText(android.app.Notification notification) {
+		// We have to extract the information from the view
+		RemoteViews views = notification.bigContentView;
+		if (views == null)
+			views = notification.contentView;
+		if (views == null)
+			return null;
 
-	    // Use reflection to examine the m_actions member of the given RemoteViews object.
-	    // It's not pretty, but it works.
-	    List<String> text = new ArrayList<String>();
-	    try
-	    {
-	        Field field = views.getClass().getDeclaredField("mActions");
-	        field.setAccessible(true);
+		// Use reflection to examine the m_actions member of the given
+		// RemoteViews object.
+		// It's not pretty, but it works.
+		List<String> text = new ArrayList<String>();
+		try {
+			Field field = views.getClass().getDeclaredField("mActions");
+			field.setAccessible(true);
 
-	        @SuppressWarnings("unchecked")
-	        ArrayList<Parcelable> actions = (ArrayList<Parcelable>) field.get(views);
+			@SuppressWarnings("unchecked")
+			ArrayList<Parcelable> actions = (ArrayList<Parcelable>) field
+					.get(views);
 
-	        // Find the setText() and setTime() reflection actions
-	        for (Parcelable p : actions)
-	        {
-	            Parcel parcel = Parcel.obtain();
-	            p.writeToParcel(parcel, 0);
-	            parcel.setDataPosition(0);
+			// Find the setText() and setTime() reflection actions
+			for (Parcelable p : actions) {
+				Parcel parcel = Parcel.obtain();
+				p.writeToParcel(parcel, 0);
+				parcel.setDataPosition(0);
 
-	            // The tag tells which type of action it is (2 is ReflectionAction, from the source)
-	            int tag = parcel.readInt();
-	            if (tag != 2) continue;
+				// The tag tells which type of action it is (2 is
+				// ReflectionAction, from the source)
+				int tag = parcel.readInt();
+				if (tag != 2)
+					continue;
 
-	            // View ID
-	            parcel.readInt();
+				// View ID
+				parcel.readInt();
 
-	            String methodName = parcel.readString();
-	            if (methodName == null) continue;
+				String methodName = parcel.readString();
+				if (methodName == null)
+					continue;
 
-	            // Save strings
-	            else if (methodName.equals("setText"))
-	            {
-	                // Parameter type (10 = Character Sequence)
-	                parcel.readInt();
+				// Save strings
+				else if (methodName.equals("setText")) {
+					// Parameter type (10 = Character Sequence)
+					parcel.readInt();
 
-	                // Store the actual string
-	                String t = TextUtils.CHAR_SEQUENCE_CREATOR.createFromParcel(parcel).toString().trim();
-	                text.add(t);
-	            }
+					// Store the actual string
+					String t = TextUtils.CHAR_SEQUENCE_CREATOR
+							.createFromParcel(parcel).toString().trim();
+					text.add(t);
+				}
 
-	            // Save times. Comment this section out if the notification time isn't important
-	            else if (methodName.equals("setTime"))
-	            {
-	                // Parameter type (5 = Long)
-	                parcel.readInt();
+				// Save times. Comment this section out if the notification time
+				// isn't important
+				else if (methodName.equals("setTime")) {
+					// Parameter type (5 = Long)
+					parcel.readInt();
 
-	                String t = new SimpleDateFormat("h:mm a").format(new Date(parcel.readLong()));
-	                text.add(t);
-	            }
+					String t = new SimpleDateFormat("h:mm a").format(new Date(
+							parcel.readLong()));
+					// text.add(t);
+				}
 
-	            parcel.recycle();
-	        }
-	    }
+				parcel.recycle();
+			}
+		}
 
-	    // It's not usually good style to do this, but then again, neither is the use of reflection...
-	    catch (Exception e)
-	    {
-	        Log.e("NotificationClassifier", e.toString());
-	    }
+		// It's not usually good style to do this, but then again, neither is
+		// the use of reflection...
+		catch (Exception e) {
+			Log.e("NotificationClassifier", e.toString());
+		}
 
-	    return text;
+		return text;
 	}
+
 	public static final class Constants {
 
 		public static final String EXTRA_MESSAGE = "extra_message";
